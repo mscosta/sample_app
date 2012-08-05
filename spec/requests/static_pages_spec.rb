03365f90dff701203b26372769359941aac79930
Matheus S. Costa <matheus.silco@gmail.com>
2012-08-05 15:03:30 -0300

    Add contact and DRY controller spec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe "Static Pages" do
+
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
@@ -11,7 +14,7 @@ describe "Static Pages" do
     it "should have the right title" do
       visit '/static_pages/home'
       page.should have_selector('title',
-      					:text => "Ruby on Rails Tutorial Sample App | Home")
+      					:text => "#{base_title} | Home")
     end						
   end
 
@@ -25,7 +28,7 @@ describe "Static Pages" do
   	it "should have the right title" do
       visit '/static_pages/help'
       page.should have_selector('title',
-      					:text => "Ruby on Rails Tutorial Sample App | Help")
+      					:text => "#{base_title} | Help")
     end			
   end
 
@@ -39,9 +42,16 @@ describe "Static Pages" do
     it "should have the right title" do
       visit '/static_pages/about'
       page.should have_selector('title',
-      					:text => "Ruby on Rails Tutorial Sample App | About Us")
+      					:text => "#{base_title} | About Us")
     end			
   end
 
+  describe "Contact page" do
 
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      page.should have_selector('title',
+                :text => "#{base_title} | Contact")
+    end
+  end 
 end
